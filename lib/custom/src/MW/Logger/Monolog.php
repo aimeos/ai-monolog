02f5b8bfb6cce61f3c7311efb60a65554a1f9751
Aimeos <aimeos@aimeos.org>
2016-10-04 10:51:38 +0200

    Fixed documentation

--- lib/custom/src/MW/Logger/Monolog.php
+++ lib/custom/src/MW/Logger/Monolog.php
@@ -41,7 +41,7 @@ class Monolog
 	/**
 	 * Writes a message to the configured log facility.
 	 *
-	 * @param string $message Message text that should be written to the log facility
+	 * @param string|array|object $message Message text that should be written to the log facility
 	 * @param integer $priority Priority of the message for filtering
 	 * @param string $facility Facility for logging different types of messages (e.g. message, auth, user, changelog)
 	 * @throws \Aimeos\MW\Logger\Exception If an error occurs in Zend_Log
